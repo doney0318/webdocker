eed1706e16a590e4ab7c0c2bfe0c88a84b002c5b
doney0318 <doney_0318@msn.com>
2019-05-31 15:29:45 +0800

    Update qiaohu.sh

--- qiaohu.sh
+++ qiaohu.sh
@@ -85,7 +85,7 @@ notice(){
     white "以下内容必须一步步操作"
     greenbg "等待数据库完成初始化，等待约10s"
     sleep 12s
-    docker exec -it qiaohu_mysql_1 mysql mysql -uroot -p$rootpwd < /tmp/qiaohu.sql
+    docker exec -it qizhimysql mysql -uroot -p$rootpwd < /tmp/qiaohu.sql
     sleep 10s
     greenbg "正在重启qiaohu"
     restart_qiaohu
