9a36c44db2e4a18bc2439ab8c2de27ccf528254a
doney0318 <doney_0318@msn.com>
2019-06-11 13:28:04 +0800

    Update qiaohu.sh

--- qiaohu.sh
+++ qiaohu.sh
@@ -84,7 +84,7 @@ notice(){
     green "=================================================="
     white "以下内容必须一步步操作"
     sleep 2s
-    docker exec -it qizhimysql mysql -uroot -p$rootpwd sq_qiaohu < /opt/qiaohu.sql
+    docker exec -it qizhimysql mysql -uroot -p$rootpwd sq_qiaohu < /opt/qiaohu/docker/database/qiaohu.sql
     greenbg "等待数据库完成初始化，等待约10s"
     sleep 12s
     greenbg "正在重启qiaohu"
